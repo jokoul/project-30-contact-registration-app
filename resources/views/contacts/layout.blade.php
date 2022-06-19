422dd02acae14b6f58ab31a9c4a43fa4e32b3f86
jokoul <74833985+jokoul@users.noreply.github.com>
2022-06-19 20:16:23 +0200

    end of the project - optimization

--- resources/views/contacts/layout.blade.php
+++ resources/views/contacts/layout.blade.php
@@ -3,9 +3,18 @@
     <head>
         <title>Contact Register</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!--STYLE-->
+        <link rel="stylesheet" href="<?php echo asset('css/style.css') ?>"/>
+         <!--Fontawesome-->
+        <script
+        src="https://kit.fontawesome.com/52339f9582.js"
+        crossorigin="anonymous"
+        ></script>
+        <!--favicon-->
+        <link rel="shortcut icon" type="image/png"  href="./img/logomakr.png"/>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info mb-5">
             <div class="container-fluid">
                 <h1><a class="navbar-brand" href="{{ url('contacts') }}">Contact Register</a></h1>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,9 +32,40 @@
                 </div>
             </div>
         </nav>
-        <div class="container">
+        <div class="container wrapper">
             @yield('content')
         </div>
+        <!--FOOTER-->
+    <footer class="footer">
+      <p>Contact Register, Copyright &copy; 2022 develop by Joan Kouloumba</p>
+      <div>
+        <ul class="social-media">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/joan-kouloumba-570a7680/"
+              target="_blank"
+              ><i class="fa-brands fa-linkedin-in"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/joanKouloumba" target="_blank"
+              ><i class="fa-brands fa-twitter"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://github.com/jokoul" target="_blank"
+              ><i class="fa-brands fa-github"></i
+            ></a>
+          </li>
+        </ul>
+        <p>
+          <a
+            href="https://joan-kouloumba.in/professional-site/index.html#achievements"
+            >Visit more site like this on the professional site.</a
+          >
+        </p>
+      </div>
+    </footer>
         <!--Bootstrap JS-->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/contacts');// redirect to contact on route "/".
 });
 
 Route::resource('contacts', ContactController::class);
