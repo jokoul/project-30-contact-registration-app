68ad7f6d874107d35473c3db568ca545393ee797
jokoul <74833985+jokoul@users.noreply.github.com>
2022-06-20 10:41:05 +0200

    fix some bug and set footer

--- resources/views/contacts/layout.blade.php
+++ resources/views/contacts/layout.blade.php
@@ -11,61 +11,63 @@
         crossorigin="anonymous"
         ></script>
         <!--favicon-->
-        <link rel="shortcut icon" type="image/png"  href="./img/logomakr.png"/>
+        <link rel="shortcut icon" type="image/png"  href="<?php echo asset('img/logomakr.png') ?>"/>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-info mb-5">
-            <div class="container-fluid">
-                <h1><a class="navbar-brand" href="{{ url('contacts') }}">Contact Register</a></h1>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ url('contacts') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/contacts/create') }}">Create Contact</a>
-                        </li>
-                    </ul>
+        <main class="content">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-info mb-5">
+                <div class="container-fluid">
+                    <h1><a class="navbar-brand" href="{{ url('contacts') }}">Contact Register</a></h1>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ url('contacts') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/contacts/create') }}">Create Contact</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+            </nav>
+            <div class="container">
+                @yield('content')
             </div>
-        </nav>
-        <div class="container wrapper">
-            @yield('content')
-        </div>
+        </main>
         <!--FOOTER-->
-    <footer class="footer">
-      <p>Contact Register, Copyright &copy; 2022 develop by Joan Kouloumba</p>
-      <div>
-        <ul class="social-media">
-          <li>
+        <footer class="footer">
+        <p>Contact Register, Copyright &copy; 2022 develop by Joan Kouloumba</p>
+        <div>
+            <ul class="social-media">
+            <li>
+                <a
+                href="https://www.linkedin.com/in/joan-kouloumba-570a7680/"
+                target="_blank"
+                ><i class="fa-brands fa-linkedin-in"></i
+                ></a>
+            </li>
+            <li>
+                <a href="https://twitter.com/joanKouloumba" target="_blank"
+                ><i class="fa-brands fa-twitter"></i
+                ></a>
+            </li>
+            <li>
+                <a href="https://github.com/jokoul" target="_blank"
+                ><i class="fa-brands fa-github"></i
+                ></a>
+            </li>
+            </ul>
+            <p>
             <a
-              href="https://www.linkedin.com/in/joan-kouloumba-570a7680/"
-              target="_blank"
-              ><i class="fa-brands fa-linkedin-in"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://twitter.com/joanKouloumba" target="_blank"
-              ><i class="fa-brands fa-twitter"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://github.com/jokoul" target="_blank"
-              ><i class="fa-brands fa-github"></i
-            ></a>
-          </li>
-        </ul>
-        <p>
-          <a
-            href="https://joan-kouloumba.in/professional-site/index.html#achievements"
-            >Visit more site like this on the professional site.</a
-          >
-        </p>
-      </div>
-    </footer>
+                href="https://joan-kouloumba.in/professional-site/index.html#achievements"
+                >Visit more site like this on the professional site.</a
+            >
+            </p>
+        </div>
+        </footer>
         <!--Bootstrap JS-->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
